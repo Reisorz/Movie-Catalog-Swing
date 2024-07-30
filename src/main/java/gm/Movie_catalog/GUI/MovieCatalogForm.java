1c98143aee078e6411ac399b8c9d2dc388283541
Reisorz <166374263+Reisorz@users.noreply.github.com>
2024-07-30 10:05:32 +0200

    Method guardarPelicula from MovieCatalogForm done.

--- src/main/java/gm/Movie_catalog/GUI/MovieCatalogForm.java
+++ src/main/java/gm/Movie_catalog/GUI/MovieCatalogForm.java
@@ -8,12 +8,22 @@ import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 @Component
 public class MovieCatalogForm extends JFrame {
     private JPanel panelPrincipal;
     private JTable peliculasTabla;
+    private JLabel nombre;
+    private JTextField nombreTexto;
+    private JButton guardarButton;
+    private JLabel year;
+    private JTextField yearTexto;
+    private JTextField puntuacionTexto;
+    private JButton eliminarButton;
+    private JButton limpiarButton;
     IPeliculaServicio peliculaServicio;
     private DefaultTableModel tablaModeloPeliculas;
 
@@ -21,7 +31,10 @@ public class MovieCatalogForm extends JFrame {
     public MovieCatalogForm(PeliculaServicio peliculaServicio) {
         this.peliculaServicio = peliculaServicio;
         iniciarForma();
+        guardarButton.addActionListener(e ->  guardarPelicula());
     }
+
+
 
     private void iniciarForma() {
         this.setContentPane(panelPrincipal);
@@ -53,5 +66,37 @@ public class MovieCatalogForm extends JFrame {
             };
             this.tablaModeloPeliculas.addRow(renglonPelicula);
         });
+    }
+
+    private void guardarPelicula() {
+        if(nombreTexto.getText().equals("")){
+            mostrarMensaje("Proporciona un nombre.");
+            nombreTexto.requestFocusInWindow();
+            return;
+        }
+        Pelicula peliculaGuardar = new Pelicula();
+        String nombrePelicula = this.nombreTexto.getText();
+        int yearPelicula = Integer.parseInt(this.yearTexto.getText());
+        Double puntuacionPelicula = Double.parseDouble(this.puntuacionTexto.getText());
+        peliculaGuardar.setNombre(nombrePelicula);
+        peliculaGuardar.setYear(yearPelicula);
+        peliculaGuardar.setPuntuacion(puntuacionPelicula);
+        peliculaServicio.guardarPelicula(peliculaGuardar);
+        mostrarMensaje("Película guardada con éxito.");
+        limpiarTexto();
+        listarPeliculas();
+
+
+
+    }
+
+    private void mostrarMensaje(String mensaje) {
+        JOptionPane.showMessageDialog(this, mensaje);
+    }
+
+    private void limpiarTexto() {
+        this.nombreTexto.setText("");
+        this.yearTexto.setText("");
+        this.puntuacionTexto.setText("");
     }
 }
